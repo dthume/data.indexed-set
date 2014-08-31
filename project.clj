93bc094f20f49eee347f6a55ab4e568d95eb76b9
David Thomas Hume <dth@dthu.me>
2014-08-31 18:52:38 +0100

    Renamed artifact id to work around artifactory wierdness

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.dthume/data.indexed-set "0.1.0-SNAPSHOT"
+(defproject org.dthume/data.set-db "0.1.0-SNAPSHOT"
   :description "Indexed set type for clojure"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-tuple "0.1.6"]
