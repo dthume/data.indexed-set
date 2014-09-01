d2b2a3e6ab83f86f5dc838794455823df4be4d51
David Thomas Hume <dth@dthu.me>
2014-09-01 10:51:49 +0100

    Add project url and license

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject org.dthume/data.set-db "0.1.0-SNAPSHOT"
   :description "Indexed set type for clojure"
+  :url "http://github.com/dthume/data.indexed-set"
+  :license "Eclipse Public License 1.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-tuple "0.1.6"]
                  [org.dthume/data.set "0.1.0"]]
