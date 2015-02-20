33a92056e138e6278ab3ce270bd42db8fbacf3f7
David Thomas Hume <dth@dthu.me>
2015-02-20 10:59:16 +0000

    Prepare release 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.dthume/data.set-db "0.1.1-SNAPSHOT"
+(defproject org.dthume/data.set-db "0.1.1"
   :description "Indexed set type for clojure"
   :url "http://github.com/dthume/data.indexed-set"
   :license "Eclipse Public License 1.0"
